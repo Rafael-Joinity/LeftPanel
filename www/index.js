7e908f0c19a2349556efbf574f094a608a72627f
Rafael-Joinity <rafael@joinity.com>
2015-02-06 17:30:16 +0100

    order

--- www/index.js
+++ www/index.js
@@ -28,6 +28,16 @@ var app = {
     bindEvents: function() {
       document.addEventListener('deviceready', app.onDeviceReady, false);
       alert('bindEvents ')
+
+        var panel = new swipe_panel({
+            elementTop      : 100,
+            elementDisabled : true,
+            onTouch         : 'patata3',
+            onTouchFinish   : 'patata4'
+        });
+        panel.initSwipe();
+
+        alert(panel)
     },
     // deviceready Event Handler
     //
@@ -40,15 +50,5 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         alert('receivedEvent')
-
-        var panel = new swipe_panel({
-            elementTop      : 100,
-            elementDisabled : true,
-            onTouch         : 'patata3',
-            onTouchFinish   : 'patata4'
-        });
-        panel.initSwipe();
-
-        alert(panel)
     }
 };
